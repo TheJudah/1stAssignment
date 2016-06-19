b57b4ad4b1a0e2ad6f5c215b5c828ef7464fd4ed
Judah <judahtravis@gmail.com>
2016-06-19 00:06:33 -0700

    Add files via upload

---
+++ App_Code/DataClass.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+/// <summary>
+/// summary for DataClass
+/// </summary>
+public class BooksAndAuthors
+{
+    //connects to the database
+    SqlConnection connect;
+
+    public BooksAndAuthors()
+    {
+        connect = new SqlConnection(ConfigurationManager
+            .ConnectionStrings["BookReviewDbConnectionString"].ToString() );
+    }
+    /// <summary>
+    /// Called in aspx.cs file.
+    /// gets authors and fills dropdown
+    /// </summary>
+    /// <returns>DataTable witn names of authors</returns>
+    public DataTable GetAuthors()
+    {
+        //sql command
+        string sql = 
+            "SELECT AuthorKey, AuthorName FROM Author ORDER BY authorname";
+        //new datatable object
+        DataTable table;
+        //Connects to db
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        try
+        {
+            //execute the sql command. Store result in table.
+            table = ProcessQuery(cmd);
+        } catch(Exception excep)
+        {
+            //Error!
+            throw excep;
+        }
+        //return table
+        return table;
+    }
+
+    public DataTable GetBooksByAuthor(int authorKey)
+    {
+        DataTable table = null;
+        //SQL command
+        String sql = " SELECT * FROM book " +
+            " INNER JOIN authorbook " +
+            " ON book.bookkey = authorbook.bookkey " +
+            " WHERE authorkey = @authorkey";
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        //put variable into sql
+        cmd.Parameters.AddWithValue("@authorkey", authorKey);
+        try
+        {
+            table = ProcessQuery(cmd);
+        } catch (Exception ex)
+        {
+            throw ex;
+        }
+        return table;
+    }
+
+    private DataTable ProcessQuery(SqlCommand cmd)
+    {
+        //table to hold data
+        DataTable table = new DataTable();
+        //object to read database
+        SqlDataReader reader;
+        try
+        {
+            //connects to server
+            connect.Open();
+            //execute
+            reader = cmd.ExecuteReader();
+            //put data in the table
+            table.Load(reader);
+            //ccose connection and reader
+            connect.Close();
+            reader.Close();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        return table;
+    }
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page
+{
+    //creates object connects to database server
+    BooksAndAuthors ba = new BooksAndAuthors();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            FillDropDownList();
+        } 
+    }//End Page_Load
+
+    
+
+    protected void FillDropDownList()
+    {
+        DataTable table = null;
+        try
+        {
+            table = ba.GetAuthors();
+        } catch (Exception ex)
+        {
+            throw ex;
+        }
+        //put data from database into dropdown list
+        DropDownList1.DataSource = table;
+        DropDownList1.DataTextField = "AuthorName";
+        DropDownList1.DataValueField = "AuthorKey";
+        DropDownList1.DataBind();
+        DropDownList1.Items.Insert(0, "Choose a Service");   
+    }
+
+    protected void FillGridView()
+    {
+        //Table to store books
+        DataTable booksTable = null;
+        //Get author key from dropdown list.
+        int authorKey = int.Parse(DropDownList1.SelectedValue.ToString() );
+        try
+        {
+            booksTable = ba.GetBooksByAuthor(authorKey);
+            //debugging
+            //if (booksTable.Rows.Count > 0)
+            //{
+            //    throw new Exception();
+            //}
+        } catch (Exception ex)
+        {
+            throw ex;
+        }
+        //GredView1
+        GridView1.DataSource = booksTable;
+        GridView1.DataBind();
+    }//End FillGridView1
+
+    protected void DropDownList1_SelectedIndexChanged1(object sender, EventArgs e)
+    {
+        FillGridView();
+    }
+}
